f798b29930d3e8897d11ce8ec64f8ed0b60a7aae
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-01-31 19:36:52 +0900

    Import

---
+++ Casks/coreclr.rb
@@ -0,0 +1,11 @@
+cask "coreclr" do
+  version "latest"
+  url "https://dotnetcli.blob.core.windows.net/dotnet/dev/Installers/Latest/dotnet-osx-x64.#{version}.pkg"
+  name ".NET Core"
+  sha256 :no_check
+  homepage "https://dotnet.github.io/"
+  license :mit
+  pkg "dotnet-osx-x64.#{version}.pkg"
+
+  uninstall pkgutil: 'com.microsoft.dotnet.cli.pkg.dotnet-osx-x64.*'
+end
---
+++ README.md
@@ -0,0 +1,4 @@
+Coreclr cask for homebrew-cask
+===
+
+An unofficial coreclr cask for homebrew-cask.
